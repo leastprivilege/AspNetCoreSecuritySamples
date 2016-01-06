fe79529c21bf013d05987cd6828ca59e1f4ef95a
Dominick Baier <dbaier@leastprivilege.com>
2016-01-06 12:16:31 +0100

    ..and more

---
+++ External/src/AspNet5Authentication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+
+namespace AspNet5Authentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secure()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Start/src/AspNet5Authentication/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace AspNet5Authentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string userName, string password, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (!string.IsNullOrWhiteSpace(userName) &&
+                userName == password)
+            {
+
+            }
+
+            return View();
+        }
+
+        //public async Task<IActionResult> Logout()
+        //{
+            
+        //}
+    }
+}
