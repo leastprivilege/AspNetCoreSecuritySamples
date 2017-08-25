abe4604e9d66e5815dae2d5cb79d3275f04a2c60
Dominick Baier <dbaier@leastprivilege.com>
2017-08-25 11:28:09 +0200

    added dataprotection demo

---
+++ DataProtection/src/AspNetCoreAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace AspNetCoreAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDataProtector _protector;
+
+        public HomeController(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector(GetType().FullName);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Protect(Data data)
+        {
+            data.CipherText = _protector.Protect(data.PlainText);
+
+            return View("index", data);
+        }
+
+        public IActionResult Unprotect(Data data)
+        {
+            data.PlainText = _protector.Unprotect(data.CipherText);
+
+            return View("index", data);
+        }
+    }
+
+    public class Data
+    {
+        public string PlainText { get; set; }
+        public string CipherText { get; set; }
+    }
+}
---
+++ DataProtection/src/AspNetCoreAuthentication/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace AspNetCoreAuthentication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // never persist key store to local directory, just for demo purposes
+            services.AddDataProtection()
+                .SetApplicationName("DataProtectionDemo")
+                .ProtectKeysWithDpapi()
+                .PersistKeysToFileSystem(new DirectoryInfo(Directory.GetCurrentDirectory()));
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
