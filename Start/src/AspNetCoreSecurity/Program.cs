0ff8bae62b9768311b9fd04d3630715599b5cdc7
Dominick Baier <dbaier@leastprivilege.com>
2018-06-04 08:23:56 +0200

    Start

--- Start/src/AspNetCoreSecurity/Program.cs
+++ Start/src/AspNetCoreSecurity/Program.cs
@@ -10,12 +10,11 @@ namespace AspNetCoreSecurity
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
