ba0717fe9f0eac6ff26b2eb13d93e9e86c017c60
Dominick Baier <dbaier@leastprivilege.com>
2018-06-14 13:07:45 +0200

    add client

---
+++ OidcAndApi/src/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var disco = await DiscoveryClient.GetAsync("https://demo.identityserver.io");
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            var tokenClient = new TokenClient(
+                disco.TokenEndpoint,
+                "client",
+                "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync();
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            Console.WriteLine(await apiClient.GetStringAsync("http://localhost:3308/api/test"));
+        }
+    }
+}
