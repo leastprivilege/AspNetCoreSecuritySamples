ff5f8e31029f597fc1a22c81911507716ac52402
Dominick Baier <dbaier@leastprivilege.com>
2018-06-14 13:17:51 +0200

    add header

--- OidcAndApi/src/AspNetCoreSecurity/Apis/TestController.cs
+++ OidcAndApi/src/AspNetCoreSecurity/Apis/TestController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreSecurity.Apis
 {
