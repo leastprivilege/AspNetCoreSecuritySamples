91c881bb432876d6afa3ab9c68e2af68114aa419
Dominick Baier <dbaier@leastprivilege.com>
2019-02-06 09:28:31 +0100

    Add token management sample

---
+++ AutomaticTokenManagement/src/Api/TestController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AspNetCoreSecurity.Apis
+{
+    [Route("test")]
+    [Authorize]
+    public class TestController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ AutomaticTokenManagement/src/AspNetCoreSecurity/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreSecurity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
