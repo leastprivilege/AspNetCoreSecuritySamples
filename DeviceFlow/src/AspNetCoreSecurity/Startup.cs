79974d604ff36cb66aa101d6a279b0055e769db3
Dominick Baier <dbaier@leastprivilege.com>
2019-02-08 12:55:19 +0100

    add device flow sample

---
+++ DeviceFlow/src/AspNetCoreSecurity/Startup.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AspNetCoreSecurity
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter(
+                    new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build()));
+            })
+                .AddJsonFormatters()
+                .AddAuthorization();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://demo.identityserver.io";
+                    options.Audience = "api";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
