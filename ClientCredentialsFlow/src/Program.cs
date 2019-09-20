b30bafb517701af08f6973edfe9596500dde5c7a
Dominick Baier <dbaier@leastprivilege.com>
2019-09-20 11:15:10 +0200

    add client creds sample

---
+++ ClientCredentialsFlow/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientCredentialsFlow
+{
+    class Program
+    {
+        static DiscoveryCache Cache = new DiscoveryCache("https://demo.identityserver.io");
+
+        static async Task Main(string[] args)
+        {
+            var disco = await Cache.GetAsync();
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            var tokenClient = new HttpClient();
+            var tokenResponse = await tokenClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret"
+            });
+
+            if (tokenResponse.IsError) throw new Exception(tokenResponse.Error);
+
+            // call API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            Console.WriteLine(await apiClient.GetStringAsync("https://demo.identityserver.io/api/test"));
+        }
+    }
+}
